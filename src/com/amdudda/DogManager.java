412e3a0749af9a1d0c50a2beac89a46e969721af
amdudda <tk0654wm@go.minneapolis.edu>
2015-10-01 09:49:41 -0500

    Added totalDaysWalks method to DogManager to tally the total number of walks schedule for a given day.

--- src/com/amdudda/DogManager.java
+++ src/com/amdudda/DogManager.java
@@ -14,6 +14,8 @@ public class DogManager {
         // write out weekly report of dog walks
         for (int i = 0; i<7; i++) {
             System.out.println(getDayName(i) + ":");
+            // print total number of walks for the day
+            System.out.println("Total walks to give today: " + totalDaysWalks(dogList,i));
             for (Dog j : dogList) {
                 j.writeWalkInfo(i);
             } // end run through dogList
@@ -29,16 +31,27 @@ public class DogManager {
         Dog boz = new Dog("Boz","99 Bottles Lane",bozschedule);
         int[] k9schedule = {1,0,0,1,0,1,1};  // binary ascii for 'K'
         Dog k9 = new Dog("K9","The Tardis",k9schedule);
+        int[] lassiesched = {1,1,1,1,1,1,1};
 
         dL.add(rover);
         dL.add(boz);
         dL.add(k9);
+        dL.add(new Dog("Lassie","101 Flicka Pier",lassiesched));
 
         /* debugging: proof Dog works
         rover.writeDog();
         boz.writeDog();
         rk9.writeDog();
         */
+    }
+
+    public static int totalDaysWalks(ArrayList<Dog> dL, int day) {
+        // returns the total number of walks for a given day
+        int totalwalks = 0;
+        for (Dog d:dL) {
+            totalwalks += d.getWalks(day);
+        }
+        return totalwalks;
     }
 
     public static String getDayName(int index) {
@@ -52,7 +65,7 @@ public class DogManager {
             case 4: return "Thursday";
             case 5: return "Friday";
             case 6: return "Saturday";
-            default: return "Day unknown";
+            default: return "Day unknown";  // in case we have bad data
         } // end switch
     } // end method getDayName
 
